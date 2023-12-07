57c3de044f5e7ce928dba40c4c47216db40e04ee
虎虎 <1078013818@qq.com>
2023-12-07 11:38:41 +0800

    feat: 新增 jwttool

--- model/user.go
+++ model/user.go
@@ -1,12 +1,21 @@
 package model
 
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type UserRegisterParams struct {
 	Username        string `json:"username"`
 	Password        string `json:"password"`
 	PasswordConfirm string `json:"passwordConfirm"`
 }
+type LoginParams struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
-type UserInfoType struct {
+type UserInfo struct {
 	UserId           int    `json:"user_id"`
 	Username         string `json:"username"`
 	Password         string `json:"password"`
@@ -17,4 +26,18 @@ type UserInfoType struct {
 	AccountLocked    bool   `json:"account_locked"`
 	RegistrationTime string `json:"registration_time"`
 	UserType         string `json:"user_type"`
+}
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id"`
+	Name     string             `bson:"name"`
+	Email    string             `bson:"email"`
+	Password string             `bson:"password"`
+}
+
+type UserRepository interface {
+	Create(c context.Context, user *User) error
+	Fetch(c context.Context) ([]User, error)
+	GetByEmail(c context.Context, email string) (User, error)
+	GetByID(c context.Context, id string) (User, error)
 }
